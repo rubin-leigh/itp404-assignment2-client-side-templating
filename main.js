81162352fa138c60669bf741eac0b19f1166a1eb
rubin-leigh <rubin.leigh@yahoo.com>
2019-09-03 23:19:26 -0700

    done

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-$('#search-form').on('submit', e =>  {
+Handlebars.registerHelper('formatted-num', function(num) {
+    return num.toLocaleString();
+})
+  
+const subredditTemplate = Handlebars.compile(
+    document.querySelector('#subreddit-template').innerHTML
+)
+
+$('#search-form').on('submit', async function(e)  {
     e.preventDefault();
     $('#results').html(`
         <div class="overlay-loader">
@@ -13,53 +21,13 @@ $('#search-form').on('submit', e =>  {
             </div>
         </div>`);
     let subreddit = $('#search-box').val();
-
-    let promise = fetchTimeout(5000, $.ajax({
-        type: 'GET',
-        url: `https://www.reddit.com/r/${subreddit}.json`,
-    }));
-
-
-    promise.then(json => {
-        let html = '<ul>';
-        json.data.children.forEach(thread => {
-            html += `
-                <li>
-                    <a target="_blank" href="${thread.data.url}">${thread.data.title}</a>
-                </li>
-                <ul>
-                    <li>Score: ${thread.data.score}</li>
-                    <li>Author: ${thread.data.author}</li>
-                </ul>
-            `;
-        });
-        html += '</ul>';
-
-        DOMPurify.addHook('afterSanitizeAttributes', function(node) {
-            // set all elements owning target to target=_blank
-            if ('target' in node) {
-                node.setAttribute('target','_blank');
-            }
-            // set non-HTML/MathML links to xlink:show=new
-            if (!node.hasAttribute('target') 
-                && (node.hasAttribute('xlink:href') 
-                    || node.hasAttribute('href'))) {
-                node.setAttribute('xlink:show', 'new');
-            }
-        });
-
-        let sanitizedHtml = DOMPurify.sanitize(html);
-
+    try {
+        let subreddits = await $.getJSON(`https://www.reddit.com/r/${subreddit}.json`)
+        
+        let threads = subreddits.data.children;
+        let sanitizedHtml = subredditTemplate({ threads });
         $('#results').html(sanitizedHtml);
-    })
-    .catch(error => $('#results').html('No results found.'));
+    } catch (error) {
+        $('#results').html('Oops, something went wrong.');
+    }
 });
-
-function fetchTimeout(m, p) {
-    const timeout = new Promise((resolve, reject) => {
-      setTimeout(() => {
-        reject(new Error('Request timeout'));
-      }, m);
-    });
-    return Promise.race([timeout, p]);
-  }
